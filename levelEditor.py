e104d8e43f9335c256f762bf01bd9ea3b560694c
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-22 08:20:18 +0000

    minor changes

--- levelEditor.py
+++ levelEditor.py
@@ -1,3 +1,8 @@
-import os,sys,pygame
+import os,sys,pygame,level
 
-class levelEditor:
+class LevelEditor:
+    def __init__(self,passed_level=None):
+        if(passed_level is None):
+            self.level = level.Level("swag.txt",2000,2000)
+        else:
+            self.level = passed_level
