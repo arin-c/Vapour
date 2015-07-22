53ce4f19896c87bbae2070fe17c2645be21584cb
Aaryn <On2iTAaryn@users.noreply.github.com>
2015-07-22 14:11:02 +1200

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -6,18 +6,27 @@ m1 = resX*.05
 m2 = resY*.3
 bWidth = resX-m1*2
 bHeight = resY*.1
+quit = True
 def play():
     pass
+def level():
+    pass
+def quit():
+    global quit
+    quit = False
 mainWindow.geometry(str(resX)+'x'+str(resY))
 mainWindow.wm_title('Vapour Lancher')
 
-playButton = tk.Button(mainWindow, command=play)
-levelButton = tk.Button(mainWindow, command=play)
-quitButton = tk.Button(mainWindow, command=play)
+bImage = tk.PhotoImage('Art\\Logo.png')
+backLabel = tk.Label(mainWindow, image=bImage)
+playButton = tk.Button(mainWindow, text='Play', command=play)
+levelButton = tk.Button(mainWindow, text='Level Editor', command=level)
+quitButton = tk.Button(mainWindow, text='Quit', command=quit)
 
+backLabel.place(x=0, y=0, width=resX, height=resY)
 playButton.place(x=m1, y=m2, width=bWidth, height=bHeight)
 levelButton.place(x=m1, y=m2*1.5, width=bWidth, height=bHeight)
 quitButton.place(x=m1, y=m2*2, width=bWidth, height=bHeight)
 
-while True:
+while quit:
     mainWindow.update()
