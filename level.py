284c46be45f322cf22fb17601b58038721fdc6d6
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-23 08:40:23 +0000

    Update level.py

--- level.py
+++ level.py
@@ -8,7 +8,8 @@ class Level:
         self.levelWidth = levelWidth
         self.levelHeight = levelHeight
         self.surface = surface
-        self.filePath = self.loadFile(filePath)
+        self.filePath = self.load(filePath)
+        self.save("2sweg.txt")
 
     def setSurface(self,surface):
         self.surface = surface
@@ -27,7 +28,7 @@ class Level:
                 charCounter = charCounter
         lineCounter+=1
 
-    def loadFile(self,filePath):
+    def load(self,filePath):
         print("loading file: %s"%(filePath))
         self.blockList = list()
         file = list(open(filePath))
@@ -46,8 +47,17 @@ class Level:
             lineCounter+=1
         print(self.gridList)
 
+    def save(self,filePath):
+        saveFile = open(filePath,'w')
+        for row in self.gridList:
+            for char in row:
+                saveFile.write(char)
+            saveFile.write("\n")
+
     def draw(self,surface = None):
         if(surface is None):
             surface = self.surface
         for tile in self.blockList:
-            pygame.draw.rect(self.surface,(100,100,200),(tile[0],tile[1],tile[2],tile[3]))
+            if(tile[4] == '#'): #if charID is # then draw red square
+                pygame.draw.rect(self.surface,(180,30,30),(tile[0],tile[1],tile[2],tile[3]))
+                pygame.draw.rect(self.surface,(250,30,30),(tile[0]+2,tile[1]+2,tile[2]-4,tile[3]-4))
