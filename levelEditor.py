5b31d1c3d24ce96ca9531c83ba8cb233f27604d1
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-23 08:11:22 +0000

    Update levelEditor.py

--- levelEditor.py
+++ levelEditor.py
@@ -1,8 +1,13 @@
 import os,sys,pygame,level
 
 class LevelEditor:
-    def __init__(self,passed_level=None):
+    def __init__(self,surface,passed_level=None):
+        self.surface = surface
         if(passed_level is None):
-            self.level = level.Level("swag.txt",2000,2000)
+            self.level = level.Level("testLevel.txt",2000,2000,self.surface)
         else:
             self.level = passed_level
+            self.level.setSurface(self.surface)
+
+    def draw(self):
+        self.level.draw()
