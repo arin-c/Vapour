fe4d0804ff0a10231d43f15b160176df790717a7
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-24 08:04:23 +0000

    Update demo_run.py

--- demo_run.py
+++ demo_run.py
@@ -1,12 +1,12 @@
-import os, sys, pygame,levelEditor
+import os, sys, pygame,levelEditor,player
 
 pygame.init()
 screen_width, screen_height = 500,500
 screen = pygame.display.set_mode((screen_width,screen_height))
 lEditor = levelEditor.LevelEditor(screen)
-print("created level editor")
+player = player.Player(50,50,screen)
 quit = False
-pygame.display.set_caption("Vapour")
+
 while(not quit):
     pygame.time.Clock().tick(30)
 
@@ -20,5 +20,6 @@ while(not quit):
     lEditor.update()
     screen.fill(0)
     lEditor.draw()
+    player.draw()
     pygame.display.flip()
 lEditor.getLevel().save('2sweg.txt')
