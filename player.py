3418d0b88394a36325bb03995b734832a2a421c6
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-24 09:55:54 +0000

    Update player.py

--- player.py
+++ player.py
@@ -4,8 +4,8 @@ class Player():
     def __init__(self,startX,startY,surface):
         self.x = startX
         self.y = startY
-        self.velX = 8
-        self.velY = 8
+        self.velX = 16
+        self.velY = 16
         self.cWidth = 50
         self.cHeight = 50
         self.sWidth = 50
@@ -14,6 +14,7 @@ class Player():
         self.jump = False
         self.doubleJump = False
         self.surface = surface
+        self.fall = False
 
     def setSurface(self,surface):
         self.surface = surface
@@ -23,26 +24,53 @@ class Player():
 
     def move(self,direction):
         direction = direction.upper()
-        if((direction == "UP" or direction == "JUMP") and not self.doubleJump):
+        if((direction == "UP" or direction == "JUMP") and not self.doubleJump and self.checkSpace(self.x,self.y-self.velY,direction)[0]):
             self.jump = True
-        elif(direction == "LEFT" and self.checkSpace(self.x-self.velX,self.y)):
+
+        if(direction == "LEFT" and self.checkSpace(self.x-self.velX,self.y,direction)[0]):
             self.x -= self.velX
-        elif(direction == "RIGHT" and self.checkSpace(self.x+self.velX,self.y)):
+        elif(direction == "LEFT"):
+            self.x -= self.checkSpace(self.x-self.velX,self.y,"LEFT")[1]
+
+        if(direction == "RIGHT" and self.checkSpace(self.x+self.velX,self.y,direction)[0]):
             self.x += self.velX
+        elif(direction == "RIGHT"):
+            self.x += self.checkSpace(self.x+self.velX,self.y,"RIGHT")[1]
 
     def draw(self):
         pygame.draw.rect(self.surface,(0,0,255),(self.x,self.y,self.cWidth,self.cHeight))
 
     def update(self):
-        if(self.checkSpace(self.x,self.y+self.velY)):
+        if(self.jump):
+            if(self.checkSpace(self.x,self.y-self.velY,"UP")[0]):
+                self.y-=self.velY
+            else:
+                self.y -= self.checkSpace(self.x,self.y-self.velY,"UP")[1]
+            self.velY-=1
+            if(self.velY <= 0):
+                self.jump = False
+                self.fall = True
+        elif(self.checkSpace(self.x,self.y+self.velY,"DOWN")[0] or self.fall): #if no space under or fall
             self.y+=self.velY
-            print("free space down")
-        else:
-            print("no free space down")
+            if(self.velY < 16):
+                self.velY+=1
+        if(not self.checkSpace(self.x,self.y+self.velY,"DOWN")[0]):  #if there is space under the ground
+            self.fall = False
+            self.velY = 16
+            self.y += self.checkSpace(self.x,self.y+self.velY,"DOWN")[1]
 
-    def checkSpace(self,px,py):
+    def checkSpace(self,px,py,direction):
         x,y,w,h,bID = (0,1,2,3,4)
+        gap = 0
         for block in self.level.blockList:
             if(not(block[y] >= py+self.cHeight or block[y]+block[h] <= py or block[x]+block[w] <= px or block[x] >= px+self.cWidth) and (block[bID] == '#' or block[bID] == '4' or block[bID] == '3' or block[bID] == '2' or block[bID] == '1' or block[bID] == '0')):
-                return False
-        return True
+                if(direction == "UP"):
+                    gap = self.y-(block[y]+block[h])
+                elif(direction == "DOWN"):
+                    gap = block[y]-(self.y+self.cHeight)
+                elif(direction == "LEFT"):
+                    gap = self.x-(block[x]+block[w])
+                elif(direction == "RIGHT"):
+                    gap = block[x]-(self.x+self.cWidth)
+                return False,gap
+        return True,gap
