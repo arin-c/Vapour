fe8383c81b20f1908383aa61f2e6509cc2b0b14b
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-24 08:57:13 +0000

    Update player.py

--- player.py
+++ player.py
@@ -6,8 +6,10 @@ class Player():
         self.y = startY
         self.velX = 8
         self.velY = 8
-        self.w = 50
-        self.h = 50
+        self.cWidth = 50
+        self.cHeight = 50
+        self.sWidth = 50
+        self.sHeight = 50
         self.xDir = "LEFT"
         self.jump = False
         self.doubleJump = False
@@ -23,10 +25,17 @@ class Player():
         direction = direction.upper()
         if((direction == "UP" or direction == "JUMP") and not self.doubleJump):
             self.jump = True
-        elif(direction == "LEFT"):
-            self.x -= 8
-        elif(direction == "RIGHT"):
-            self.x += 8
+        elif(direction == "LEFT" and self.checkSpace((self.x-self.velX,self.y))):
+            self.x -= self.velX
+        elif(direction == "RIGHT" and self.checkSpace((self.x+self.velX,self.y))):
+            self.x += self.velX
 
     def draw(self):
-        pygame.draw.rect(self.surface,(0,0,255),(self.x,self.y,self.w,self.h))
+        pygame.draw.rect(self.surface,(0,0,255),(self.x,self.y,self.cWidth,self.cHeight))
+
+    def checkSpace(self,(px,py)):
+        x,y,w,h,bID = (0,1,2,3,4)
+        for block in self.level.blockList:
+            if(not(block[y] >= py+self.cHeight or block[y]+block[h] <= py or block[x]+block[w] <= px or block[x] >= px+self.cWidth) and (block[bID] == '#' or block[bID] == '4' or block[bID] == '3' or block[bID] == '2' or block[bID] == '1' or block[bID] == '0')):
+                return False
+        return True
