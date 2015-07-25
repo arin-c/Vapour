31c3bf40a4af997b5847f5dd0682b937b738f9fd
Viliami Tuanaki <Viliami@users.noreply.github.com>
2015-07-25 05:34:39 +0000

    Update levelEditor.py

--- levelEditor.py
+++ levelEditor.py
@@ -7,6 +7,7 @@ class LevelEditor:
         self.camera = passed_camera
         if(passed_level is None):
             self.level = level.Level("testLevel.txt",2000,2000,self.camera,self.surface)
+            self.level.setBackground(pygame.image.load("images/sky.png"))
         else:
             self.level = passed_level
             self.level.setSurface(self.surface)
@@ -14,12 +15,22 @@ class LevelEditor:
         self.mouseX, self.mouseY = (0,0)
         self.gridMouseX,self.gridMouseY = (-1,-1)
         self.currentBID = '#'
+        self.loadUISprites()
 
     def draw(self):
         self.level.draw()
         if(self.grid):
             self.drawGrid()
         self.drawUI()
+
+    def loadUISprites(self):
+        self.sprite_grass_TL = pygame.transform.scale(pygame.image.load("images/tiles/Grass_TL.png"),(35,35))
+        self.sprite_grass_TM = pygame.transform.scale(pygame.image.load("images/tiles/Grass_TM.png"),(35,35))
+        self.sprite_grass_TR = pygame.transform.scale(pygame.image.load("images/tiles/Grass_TR.png"),(35,35))
+        self.sprite_grass_Centre = pygame.transform.scale(pygame.image.load("images/tiles/Grass_Centre.png"),(35,35))
+        self.sprite_grass_L = pygame.transform.scale(pygame.image.load("images/tiles/Grass_L.png"),(35,35))
+        self.sprite_grass_R = pygame.transform.scale(pygame.image.load("images/tiles/Grass_R.png"),(35,35))
+
 
     def getLevel(self):
         return self.level
@@ -54,7 +65,7 @@ class LevelEditor:
     def drawUI(self):
         startX,startY,uiW,uiH = (0,screen_height-85,screen_width,85)
         pygame.draw.rect(self.surface,(30,30,30),(startX,startY,uiW,uiH))
-        self.drawUIBlock((200,0,0),(startX+5,startY+5,35,35),'#')
+        self.drawUIBlock(self.sprite_grass_Centre,(startX+5,startY+5,35,35),'#')
         self.drawUIBlock((0,200,0),(startX+45,startY+5,35,35),'$')
         self.drawUIBlock((0,0,200),(startX+85,startY+5,35,35),'@')
         self.drawUIBlock((200,200,0),(startX+125,startY+5,35,35),'!')
@@ -62,10 +73,14 @@ class LevelEditor:
 
     def drawUIBlock(self,color,rect,bID):
         x,y,w,h = (0,1,2,3)
-        if(self.mouseX >= rect[x] and self.mouseX <= rect[x]+rect[w] and self.mouseY >= rect[y] and self.mouseY <= rect[y]+rect[h]):
-            pygame.draw.rect(self.surface,color,rect)
-            if(pygame.mouse.get_pressed()[0]):
-                self.currentBID = bIDs
-                print("bid = %s"%(bID))
-        else:
-            pygame.draw.rect(self.surface,(color[0]*0.6,color[1]*0.6,color[2]*0.6),rect)
+        print(type(color))
+        if(type(color) is tuple):
+            if(self.mouseX >= rect[x] and self.mouseX <= rect[x]+rect[w] and self.mouseY >= rect[y] and self.mouseY <= rect[y]+rect[h]):
+                pygame.draw.rect(self.surface,color,rect)
+                if(pygame.mouse.get_pressed()[0]):
+                    self.currentBID = bID
+                    print("bid = %s"%(bID))
+            else:
+                pygame.draw.rect(self.surface,(color[0]*0.6,color[1]*0.6,color[2]*0.6),rect)
+        elif(type(color) is pygame.Surface):
+            self.surface.blit(color,(rect[0],rect[1]))
